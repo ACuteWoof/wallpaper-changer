bfa5581a0934feb6d1c53c7547dafab0941f9fa8
ACuteWoof <sutharshanvithushan@gmail.com>
2021-09-12 14:01:06 +0530

    update for pkging

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from distutils.core import setup
 setup(
         name="Woof Wallpaper Changer",
         version="1.0",
-        py_modules=["changeqtilewall"]
+        py_modules=["woofchangewall"]
         )
